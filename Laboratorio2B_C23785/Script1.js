7325bcdb87dcf9c84210e8a4e8a02a39a1c13663
andyhr12004 <andy.hernandezrojas@ucr.ac.cr>
2025-03-25 23:20:54 -0600

    se agregan las funciones de borrar elemento y cambiar de fondo, últimas funciones del js en funcionamiento.

--- Laboratorio2B_C23785/Script1.js
+++ Laboratorio2B_C23785/Script1.js
@@ -11,3 +11,22 @@ function agregar() { //agrega nuevo elemento a la lista y toma el numero anterio
     lista.appendChild(nuevoElemento);
 }
 
+// funcion de cambiar color de fondo
+function cambiarFondo() {
+    const body = document.body;
+    // se fija que color hay puesto y lo cambia por el otro, en este caso rojoo
+    if (body.style.backgroundColor === 'red') {
+        body.style.backgroundColor = 'white';
+    } else {
+        body.style.backgroundColor = 'red';
+    }
+}
+
+// Funcion que elimina el ultimo elemento
+function borrar() {
+    const lista = document.getElementById('lista');
+    // borra el ultimo espacio si este no está vacío
+    if (lista.children.length > 0) {
+        lista.removeChild(lista.lastElementChild);
+    }
+}
