c8003c0085503c83a270d74a0069406d7cb5a2ed
andyhr12004 <andy.hernandezrojas@ucr.ac.cr>
2025-04-13 18:37:48 -0600

    Se avanzan las partes 1 y 2 del LAB4 de backend, se conectó al servidos y se creó la tabla en la base de datos

---
+++ backend-lab-c23785/backend-lab-c23785/Controllers/PaisesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_c23785.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
